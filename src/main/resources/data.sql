97c5075b434d37514e0b3d18769a8aa97a72a385
Ephraim <712020:b306d4cd-34ab-4c03-a883-027b3dc54286@bots.bitbucket.org>
2023-07-14 12:57:11 +0200

    #2 Patient API - Add import data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+
+INSERT INTO patients(national_identification_number, full_name, gender, date_of_birth) VALUES
+	('42-267619N42', 'Ephraim Madondo', 'MALE','1995-01-01 00:00:00'),
+	('43-267619M42', 'TestUser Patient', 'MALE','1995-01-01 00:00:00');
